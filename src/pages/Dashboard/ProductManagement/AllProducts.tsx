b30389eeacdf9ee6cd928c186f7d0068b8ab0b0f
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-01-29 13:06:03 +0600

    category filter functionality added

--- src/pages/Dashboard/ProductManagement/AllProducts.tsx
+++ src/pages/Dashboard/ProductManagement/AllProducts.tsx
@@ -17,7 +17,7 @@ type TBook = {
   _id: string;
 };
 const AllProducts = () => {
-  const [activeTab, setActiveTab] = useState("tab1");
+  const [activeTab, setActiveTab] = useState("all");
   const { data, isLoading } = useGetAllBookDataQuery(undefined);
   // console.log(data);
 
@@ -28,6 +28,16 @@ const AllProducts = () => {
       </div>
     );
   }
+
+  const fictonBookData = data?.data?.filter(
+    (item: TBook) => item.category === "fiction"
+  );
+  const educationBookData = data?.data?.filter(
+    (item: TBook) => item.category === "academic"
+  );
+  const childrenBookData = data?.data?.filter(
+    (item: TBook) => item.category === "children"
+  );
   return (
     <div>
       <div className="text-white text-center">
@@ -38,81 +48,200 @@ const AllProducts = () => {
           <div className="flex border-b border-gray-300">
             <button
               className={`px-4 py-2 text-sm font-medium ${
-                activeTab === "tab1"
+                activeTab === "all"
                   ? "text-white bg-blue-600"
                   : "text-gray-600 hover:text-blue-600"
               }`}
-              onClick={() => setActiveTab("tab1")}
+              onClick={() => setActiveTab("all")}
             >
-              Tab 1
+              All
             </button>
             <button
               className={`px-4 py-2 text-sm font-medium ${
-                activeTab === "tab2"
+                activeTab === "fiction"
                   ? "text-white bg-blue-600"
                   : "text-gray-600 hover:text-blue-600"
               }`}
-              onClick={() => setActiveTab("tab2")}
+              onClick={() => setActiveTab("fiction")}
             >
-              Tab 2
+              Fiction
             </button>
             <button
               className={`px-4 py-2 text-sm font-medium ${
-                activeTab === "tab3"
+                activeTab === "education"
                   ? "text-white bg-blue-600"
                   : "text-gray-600 hover:text-blue-600"
               }`}
-              onClick={() => setActiveTab("tab3")}
+              onClick={() => setActiveTab("education")}
             >
-              Tab 3
+              Education
+            </button>
+            <button
+              className={`px-4 py-2 text-sm font-medium ${
+                activeTab === "children"
+                  ? "text-white bg-blue-600"
+                  : "text-gray-600 hover:text-blue-600"
+              }`}
+              onClick={() => setActiveTab("children")}
+            >
+              Children
             </button>
           </div>
           <div className="mt-4">
-            {activeTab === "tab1" && (
-              <div className="p-4 bg-gray-100 rounded">Content for Tab 1</div>
+            {activeTab === "all" && (
+              <div className="p-4  rounded">
+                {/*All Books cards */}
+                <div className="grid lg:grid-cols-3 gap-5 px-5 md:grid-cols-2 grid-cols-1">
+                  {data?.data?.map((item: TBook) => (
+                    <div
+                      key={item._id}
+                      className="bg-gradient-to-b from-[#1B1B31] via-[#2B1E36] to-[#1B1B31] p-6 rounded-lg shadow-lg text-white "
+                    >
+                      <div className="flex items-center justify-center mb-4">
+                        <img
+                          src={item?.imageUrl}
+                          alt="Book Cover"
+                          className="w-44 h-40 object-cover rounded-md shadow-md"
+                        />
+                      </div>
+                      <h3 className="text-xl font-semibold mb-2">
+                        {item?.title.slice(0, 30)} ...
+                      </h3>
+                      <p className="text-gray-300 text-sm mb-4">
+                        {item.description.slice(0, 200)} ...
+                      </p>
+                      <div className="flex justify-between items-center">
+                        <span className="text-lg font-bold text-blue-400">
+                          ট {item?.price}
+                        </span>
+                        <Link
+                          to={`/product-details/${item._id}`}
+                          className="px-4 py-2  text-sm font-medium transition text-white bg-gradient-to-r from-purple-500 to-blue-500 rounded-lg hover:from-blue-500 hover:to-purple-500 focus:outline-none"
+                        >
+                          View Details
+                        </Link>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
             )}
-            {activeTab === "tab2" && (
-              <div className="p-4 bg-gray-100 rounded">Content for Tab 2</div>
+            {activeTab === "fiction" && (
+              <div className="p-4  rounded">
+                {/*Fiction Books cards */}
+                <div className="grid lg:grid-cols-3 gap-5 px-5 md:grid-cols-2 grid-cols-1">
+                  {fictonBookData?.map((item: TBook) => (
+                    <div
+                      key={item._id}
+                      className="bg-gradient-to-b from-[#1B1B31] via-[#2B1E36] to-[#1B1B31] p-6 rounded-lg shadow-lg text-white "
+                    >
+                      <div className="flex items-center justify-center mb-4">
+                        <img
+                          src={item?.imageUrl}
+                          alt="Book Cover"
+                          className="w-44 h-40 object-cover rounded-md shadow-md"
+                        />
+                      </div>
+                      <h3 className="text-xl font-semibold mb-2">
+                        {item?.title.slice(0, 30)} ...
+                      </h3>
+                      <p className="text-gray-300 text-sm mb-4">
+                        {item.description.slice(0, 200)} ...
+                      </p>
+                      <div className="flex justify-between items-center">
+                        <span className="text-lg font-bold text-blue-400">
+                          ট {item?.price}
+                        </span>
+                        <Link
+                          to={`/product-details/${item._id}`}
+                          className="px-4 py-2  text-sm font-medium transition text-white bg-gradient-to-r from-purple-500 to-blue-500 rounded-lg hover:from-blue-500 hover:to-purple-500 focus:outline-none"
+                        >
+                          View Details
+                        </Link>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
             )}
-            {activeTab === "tab3" && (
-              <div className="p-4 bg-gray-100 rounded">Content for Tab 3</div>
+            {activeTab === "education" && (
+              <div className="p-4 rounded">
+                {/*Education Books cards */}
+                <div className="grid lg:grid-cols-3 gap-5 px-5 md:grid-cols-2 grid-cols-1">
+                  {educationBookData?.map((item: TBook) => (
+                    <div
+                      key={item._id}
+                      className="bg-gradient-to-b from-[#1B1B31] via-[#2B1E36] to-[#1B1B31] p-6 rounded-lg shadow-lg text-white "
+                    >
+                      <div className="flex items-center justify-center mb-4">
+                        <img
+                          src={item?.imageUrl}
+                          alt="Book Cover"
+                          className="w-44 h-40 object-cover rounded-md shadow-md"
+                        />
+                      </div>
+                      <h3 className="text-xl font-semibold mb-2">
+                        {item?.title.slice(0, 30)} ...
+                      </h3>
+                      <p className="text-gray-300 text-sm mb-4">
+                        {item.description.slice(0, 200)} ...
+                      </p>
+                      <div className="flex justify-between items-center">
+                        <span className="text-lg font-bold text-blue-400">
+                          ট {item?.price}
+                        </span>
+                        <Link
+                          to={`/product-details/${item._id}`}
+                          className="px-4 py-2  text-sm font-medium transition text-white bg-gradient-to-r from-purple-500 to-blue-500 rounded-lg hover:from-blue-500 hover:to-purple-500 focus:outline-none"
+                        >
+                          View Details
+                        </Link>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+            {activeTab === "children" && (
+              <div className="p-4 rounded">
+                {" "}
+                {/*Children Books cards */}
+                <div className="grid lg:grid-cols-3 gap-5 px-5 md:grid-cols-2 grid-cols-1">
+                  {childrenBookData?.map((item: TBook) => (
+                    <div
+                      key={item._id}
+                      className="bg-gradient-to-b from-[#1B1B31] via-[#2B1E36] to-[#1B1B31] p-6 rounded-lg shadow-lg text-white "
+                    >
+                      <div className="flex items-center justify-center mb-4">
+                        <img
+                          src={item?.imageUrl}
+                          alt="Book Cover"
+                          className="w-44 h-40 object-cover rounded-md shadow-md"
+                        />
+                      </div>
+                      <h3 className="text-xl font-semibold mb-2">
+                        {item?.title.slice(0, 30)} ...
+                      </h3>
+                      <p className="text-gray-300 text-sm mb-4">
+                        {item.description.slice(0, 200)} ...
+                      </p>
+                      <div className="flex justify-between items-center">
+                        <span className="text-lg font-bold text-blue-400">
+                          ট {item?.price}
+                        </span>
+                        <Link
+                          to={`/product-details/${item._id}`}
+                          className="px-4 py-2  text-sm font-medium transition text-white bg-gradient-to-r from-purple-500 to-blue-500 rounded-lg hover:from-blue-500 hover:to-purple-500 focus:outline-none"
+                        >
+                          View Details
+                        </Link>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
             )}
           </div>
-        </div>
-
-        {/* cards */}
-        <div className="grid lg:grid-cols-3 gap-5 px-5 md:grid-cols-2 grid-cols-1">
-          {data?.data.map((item: TBook) => (
-            <div
-              key={item._id}
-              className="bg-gradient-to-b from-[#1B1B31] via-[#2B1E36] to-[#1B1B31] p-6 rounded-lg shadow-lg text-white "
-            >
-              <div className="flex items-center justify-center mb-4">
-                <img
-                  src={item?.imageUrl}
-                  alt="Book Cover"
-                  className="w-44 h-40 object-cover rounded-md shadow-md"
-                />
-              </div>
-              <h3 className="text-xl font-semibold mb-2">
-                {item?.title.slice(0, 30)} ...
-              </h3>
-              <p className="text-gray-300 text-sm mb-4">
-                {item.description.slice(0, 200)} ...
-              </p>
-              <div className="flex justify-between items-center">
-                <span className="text-lg font-bold text-blue-400">
-                  ট {item?.price}
-                </span>
-                <Link
-                  to={`/product-details/${item._id}`}
-                  className="px-4 py-2  text-sm font-medium transition text-white bg-gradient-to-r from-purple-500 to-blue-500 rounded-lg hover:from-blue-500 hover:to-purple-500 focus:outline-none"
-                >
-                  View Details
-                </Link>
-              </div>
-            </div>
-          ))}
         </div>
       </div>
     </div>
